f6e15be144a817d94b612821b9d664314292a151
Facundo Ramos <facundo@wepay.com>
2017-05-07 23:22:19 -0700

    Attempting to add coverage with codecov

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='falcon_versioning',
+      version='1.0.0',
+      long_description=open('README.md', 'r').read(),
+      license='MIT License',
+      packages=['falcon_versioning']
+      )
